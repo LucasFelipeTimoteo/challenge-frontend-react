07f355d95aad017766021977ee13f0df853ca0a0
Lucas Felipe <conhecimento54321@gmail.com>
2021-09-13 21:50:50 -0300

    Create Card part component

---
+++ frontend/src/parts/GLOBAL/Card/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import {
+  Card as CardContainer,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Typography
+} from '@material-ui/core'
+
+import wallpaper from '../../../assets/images/wallpaper.avif'
+import useStyles from './styles'
+import { People } from '@material-ui/icons'
+
+type CardProps = {
+  favorited?: boolean
+}
+
+export default function Card({ favorited }: CardProps) {
+  const {
+    addFavoritesButton,
+    disfavorButton,
+    cardMedia,
+    cardTitle,
+    cardWrapper,
+    textContentWrapper
+  } = useStyles()
+
+  return (
+    <>
+      <CardContainer className={cardWrapper}>
+        <CardMedia
+          className={cardMedia}
+          image={wallpaper}
+          title="Character image"
+        />
+
+        <IconButton
+          title={
+            favorited ? "Disfavor character" : "Add character to favorites"
+          }
+          className={favorited ? disfavorButton : addFavoritesButton}
+        >
+          <People />
+        </IconButton>
+
+        <CardContent className={textContentWrapper}>
+          <Typography
+            variant="h2"
+            component="p"
+            title="Character name"
+            className={cardTitle}
+          >
+            hero name
+          </Typography>
+
+          <Typography variant="body1" title="Character description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil rerum dolorum earum nobis quae sapiente incidunt quaerat dolore aliquid blanditiis.
+          </Typography>
+        </CardContent>
+      </CardContainer>
+    </>
+  )
+}
