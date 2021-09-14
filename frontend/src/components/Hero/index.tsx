fc431c0a058a96899bfad7f5d2153cfa1a0a4fd9
Lucas Felipe <conhecimento54321@gmail.com>
2021-09-13 21:14:14 -0300

    Create Hero component

---
+++ frontend/src/components/Hero/index.tsx
@@ -0,0 +1,70 @@
+import React, { ReactNode } from 'react'
+import { Typography, useMediaQuery, useTheme } from '@material-ui/core'
+import { KeyboardReturn } from '@material-ui/icons'
+import { useHistory } from 'react-router'
+import { useStyles } from './styles'
+
+type HeroProps = {
+  children?: ReactNode,
+  characterPage?: boolean,
+  charactersPage?: boolean,
+  teamPage?: boolean,
+}
+
+export default function Hero({
+  children,
+  characterPage,
+  charactersPage,
+  teamPage
+}: HeroProps) {
+  const { goBack } = useHistory()
+  const { breakpoints } = useTheme()
+  const query = useMediaQuery(`(min-width:${breakpoints.values.md}px)`)
+
+  const {
+    HeroContainer,
+    teamPageConditionalHeroContainer,
+    heroContentContainer,
+    characterConditionalHeroContentContainer,
+    heroText,
+    returnToPreviousPageButtonWrapper
+  } = useStyles()
+
+  return (
+    <div className={`
+      ${HeroContainer}
+      ${teamPage && teamPageConditionalHeroContainer}`} >
+      <div className={
+        `${heroContentContainer}
+         ${characterPage && characterConditionalHeroContentContainer}`
+      }>
+
+        {
+          (!charactersPage && query) && (
+            <div
+              className={returnToPreviousPageButtonWrapper}
+              onClick={goBack}
+            >
+              <Typography>
+                Return to previous page
+              </Typography>
+              <KeyboardReturn />
+            </div>
+          )
+        }
+
+        <Typography
+          variant="h1"
+          component="h2"
+          className={heroText}
+        >
+          {charactersPage && 'Explore the most powerful chacters in Marvel'}
+          {characterPage && 'Discover all comics this character took part in'}
+          {teamPage && 'Here is your own strike team choice'}
+        </Typography>
+
+        {children}
+      </div>
+    </div>
+  )
+}
