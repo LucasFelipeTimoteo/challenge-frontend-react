172a60f9e8fbc688296c00ecbd26e7512b9c718c
Lucas Felipe <conhecimento54321@gmail.com>
2021-09-13 21:09:43 -0300

    Create Routes.tsx

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Switch
+} from 'react-router-dom'
+import Footer from './components/Footer'
+import Character from './pages/Character'
+import Characters from './pages/Characters'
+import Team from './pages/Team'
+
+
+export default function Routes() {
+
+  return (
+    <BrowserRouter>      
+      <Switch>
+        <Route exact path="/">
+          <Characters />
+        </Route>
+
+        <Route path="/character">
+          <Character />
+        </Route>
+
+        <Route path="/team">
+          <Team />
+        </Route>
+
+        <Redirect from="*" to="/" />
+      </Switch>
+
+      <Footer />
+    </BrowserRouter>
+  )
+}
