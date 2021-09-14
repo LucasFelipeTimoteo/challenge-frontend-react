fb8d1bb221fe0e66cb946f08161d1ab1957cf3bf
Lucas Felipe <conhecimento54321@gmail.com>
2021-09-14 20:54:11 -0300

    Remove conditional renderinf from Logo component

--- frontend/src/parts/GLOBAL/Header/Logo/index.tsx
+++ frontend/src/parts/GLOBAL/Header/Logo/index.tsx
@@ -1,26 +1,16 @@
 import React from 'react'
-import { useMediaQuery, useTheme } from '@material-ui/core'
 import logo from '../../../../assets/SVGs/logo.svg'
 
 export default function Logo() {
-  const { breakpoints } = useTheme()
-  const query = useMediaQuery(`(min-width:${breakpoints.values.md}px)`)
-
-  if (!query) {
-    return null
-  }
-
   return (
     <>
-      {query && (
-        <img
-          src={logo}
-          width="68px"
-          height="77px"
-          alt="Logo"
-          title="Ironman face icon"
-        />
-      )}
+      <img
+        src={logo}
+        width="68px"
+        height="77px"
+        alt="Logo"
+        title="Ironman face icon"
+      />
     </>
   )
 }
