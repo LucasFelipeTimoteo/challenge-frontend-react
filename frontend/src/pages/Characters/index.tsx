48eff14ef3fc656583414bdb6aaca74fd5c75d45
Lucas Felipe <conhecimento54321@gmail.com>
2021-09-16 18:36:50 -0300

    Remove experimental  Pagination omponent import from Characters page

--- frontend/src/pages/Characters/index.tsx
+++ frontend/src/pages/Characters/index.tsx
@@ -3,7 +3,6 @@ import CharactersList from '../../components/CharactersList'
 import Header from '../../components/Header'
 import Hero from '../../components/Hero'
 import Searchbar from '../../parts/CharactersList/Searchbar'
-import CardsPagination from '../../parts/GLOBAL/CardsPagination'
 import SectionInfo from '../../parts/GLOBAL/SectionInfo'
 
 export default function Characters() {
@@ -16,7 +15,6 @@ export default function Characters() {
 
       <SectionInfo charactersPage />
       <CharactersList />
-      <CardsPagination />
     </>
   )
 }
