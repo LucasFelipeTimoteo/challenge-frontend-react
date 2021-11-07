6b52a1a2f8cb4a3160a7a4a683235208858a57c2
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 20:54:11 -0300

    Update sectionTitle style Add margin reponsiveness

--- frontend/src/parts/GLOBAL/SectionInfo/parts/SectionTitle/styles.ts
+++ frontend/src/parts/GLOBAL/SectionInfo/parts/SectionTitle/styles.ts
@@ -1,9 +1,14 @@
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(({breakpoints}) => ({
   sectionTitle: {
     fontWeight: 700,
+    marginRight: 'auto',
+
+    [breakpoints.down(breakpoints.values.md)]: {
+      marginRight: 0
+    }
   }
-})
+}))
 
 export default useStyles
