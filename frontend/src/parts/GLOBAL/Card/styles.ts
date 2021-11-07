b513e484583b739707fd69811b8a94d071e2e1e3
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 20:46:39 -0300

    Update Card component styles Add styles to new sub components

--- frontend/src/parts/GLOBAL/Card/styles.ts
+++ frontend/src/parts/GLOBAL/Card/styles.ts
@@ -1,5 +1,5 @@
 import { makeStyles } from "@material-ui/core";
-import { cardStyleVars } from "../../../themes/globalStyleVariables";
+import { cardScrollbarStyleObjectVar, cardStyleVars } from "../../../themes/globalStyleVariables";
 import { StylesObjectJSS } from "./types";
 
 const cardFavoriteOrDisfavorButtonSharedStyles: StylesObjectJSS = {
@@ -24,15 +24,33 @@ const useStyles = makeStyles(({ breakpoints, palette }) => ({
     boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
     borderRadius: '10px 30px',
     position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
 
     [breakpoints.down(breakpoints.values.xs)]: {
       width: `calc(${cardStyleVars.cardWidth}px - 16px)`
     }
   },
-  
+
   cardMedia: {
-    height: '50%',
+    minHeight: '50%',
     width: 'auto'
+  },
+
+  cardMediaOverlay: {
+    opacity: 0,
+    width: '100%',
+    height: '100%',
+    color: 'white',
+
+    '&:hover': {
+      opacity: 1,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      background: '#1a1a1ab9',
+      cursor: 'pointer'
+    }
   },
 
   addFavoritesButton: {
@@ -55,12 +73,51 @@ const useStyles = makeStyles(({ breakpoints, palette }) => ({
   },
 
   textContentWrapper: {
-    overflow: 'hidden',
-    textAlign: 'left'
+    textAlign: 'left',
+    cursor: 'pointer',
+    height: '100%'
   },
 
   cardTitle: {
     fontWeight: 600,
+    width: '90%'
+  },
+
+  cardDescriptionFullSizeMode: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    width: '100%',
+    height: '100%',
+    background: palette.background.paper,
+    zIndex: 1,
+    padding: '16px 16px 80px',
+    transition: 'background 0.7s',
+    overflow: 'auto',
+
+    ...cardScrollbarStyleObjectVar
+  },
+
+  readMoreGradientWrapper: {
+    zIndex: 1,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    minHeight: 50,
+    marginTop: 'auto',
+    width: '100%',
+    background: `linear-gradient(transparent 3px, ${palette.background.paper} 27px)`,
+
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+
+  readMoreText: {
+    fontWeight: 700,
+    color: 'gray',
+    textShadow: '0 0 1px white',
+    textTransform: 'uppercase',
   }
 }))
 
