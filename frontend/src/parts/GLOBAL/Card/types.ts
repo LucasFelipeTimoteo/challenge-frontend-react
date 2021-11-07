391138c6d246bf57472e2849d9dd19503238cd2f
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 20:45:11 -0300

    Update CardProps type to interface

--- frontend/src/parts/GLOBAL/Card/types.ts
+++ frontend/src/parts/GLOBAL/Card/types.ts
@@ -2,7 +2,7 @@ import { CreateCSSProperties, PropsFunc } from "@material-ui/styles"
 import { CSSProperties } from "react"
 import { ICharacter } from "../../../types/ICharacters/types"
 
-export type CardProps = {
+export interface ICardProps {
   character: ICharacter,
   thumbnail: string,
   extension: string,
