a8a3c4a689936135221ecab72d8f1a5a32fc7719
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 21:39:02 -0300

    Create useSearchCharactersList hook

---
+++ frontend/src/hooks/useSearchCharactersList/index.ts
@@ -0,0 +1,65 @@
+import md5 from "md5"
+import { useEffect, useState } from "react"
+import marvelApiInstance from "../../services/api"
+import getEnvVariables from "../utils/getEnvVariables"
+
+type useSearchCharactersListProps = {
+  searchKey?: string
+}
+
+export default function useSearchCharactersList({ searchKey }: useSearchCharactersListProps) {
+  const [searchCharactersList, setSearchCharactersList] = useState([])
+  const [totalSearchResults, setTotalSearchResults] = useState(0)
+  const [loadingSearchCharacters, setSoadingSearchCharacters] = useState(false)
+
+  useEffect(() => {
+    if (!searchKey) {
+      setTotalSearchResults(0)
+    }
+
+    const getSearchedCharacters = async () => {
+      setSoadingSearchCharacters(true)
+
+      const nameStartsWith = searchKey
+      const timestamp = String(Date.now())
+      const { privateKey, publicKey, limit } = getEnvVariables()
+
+      const hash = () => {
+        const hashMd5 = md5(timestamp + privateKey + publicKey)
+        return hashMd5
+      }
+
+      try {
+        const api = marvelApiInstance({
+          limit, 
+          publicKey,
+          nameStartsWith,
+          hash,
+          timestamp
+        })
+        const apiRequest = await api('characters')
+
+        const response = apiRequest.data
+        const resultsNumber = response.data.total
+        const searchedCharactersList = response.data.results
+
+        setTotalSearchResults(resultsNumber)
+        setSearchCharactersList(searchedCharactersList)
+        setSoadingSearchCharacters(false)
+      }
+      catch (error) {
+        console.log(error)
+      }
+    }
+
+    if (searchKey) {
+      getSearchedCharacters()
+    }
+  }, [searchKey])
+
+  return {
+    searchCharactersList,
+    totalSearchResults,
+    loadingSearchCharacters
+  }
+}
