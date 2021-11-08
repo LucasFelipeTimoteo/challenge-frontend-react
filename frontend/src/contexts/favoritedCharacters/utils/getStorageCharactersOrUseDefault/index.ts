562557740da31f353960f7ec4352d46e20502904
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 21:43:51 -0300

    Create getStorageFavoriteCharactersOrUseDefault util function

---
+++ frontend/src/contexts/favoritedCharacters/utils/getStorageCharactersOrUseDefault/index.ts
@@ -0,0 +1,25 @@
+import charactersListTypeGuard from "../../../../hooks/utils/typeGuards/charactersListTypeGuard"
+import { ICharacter } from "../../../../types/ICharacters/types"
+
+let defaultCharacterValue: ICharacter[] = []
+
+const getStorageFavoriteCharactersOrUseDefault = () => {
+  const storageFavoriteCharacters = window.localStorage.getItem('MARVEL_STRIKE_TEAM_FETCHED_CHARACTERS_FAVORITES')
+  if (!storageFavoriteCharacters) {
+    return defaultCharacterValue
+  }
+  const parsedStorageFavoriteCharacters = JSON.parse(storageFavoriteCharacters)
+
+  if (!Array.isArray(parsedStorageFavoriteCharacters)) {
+    return defaultCharacterValue
+  }
+
+  if (charactersListTypeGuard(parsedStorageFavoriteCharacters)) {
+    return parsedStorageFavoriteCharacters
+  }
+  else {
+    return defaultCharacterValue
+  }
+}
+
+export default getStorageFavoriteCharactersOrUseDefault
