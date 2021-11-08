8462f67bfe6148ac38d6bdfeb58a0b5a24df4613
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 21:05:52 -0300

    Create ResultsNotFound part styles

---
+++ frontend/src/parts/GLOBAL/ResultsNotFound/styles.ts
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  ResultsNotFoundContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontWeight: 800,
+    textShadow: '1px 1px 2px gray',
+    marginTop: 20
+  }
+})
+
+export default useStyles
