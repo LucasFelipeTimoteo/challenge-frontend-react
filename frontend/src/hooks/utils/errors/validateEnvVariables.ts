00e7abb353510f432d2e6a967608936a0122840f
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 21:29:22 -0300

    Create validateEnvVariables util function

---
+++ frontend/src/hooks/utils/errors/validateEnvVariables.ts
@@ -0,0 +1,27 @@
+interface IThrowEnvErrorsprops {
+  envPrivateKey: string | undefined,
+  envPublicKey: string | undefined,
+  envLimit: string | undefined
+}
+
+const validateEnvVariables = ({ envPrivateKey, envPublicKey, envLimit }: IThrowEnvErrorsprops) => {
+  if (!envPrivateKey) {
+    throw new Error('"PrivateKey" env variable cannot be accessed.');
+  }
+  if (!envPublicKey) {
+    throw new Error('"PublicKey" env variable cannot be accessed.');
+  }
+  if (!envLimit) {
+    throw new Error('"Limit" env variable cannot be accessed.');
+  }
+
+  const validatedEnvVariables = {
+    privateKey: envPrivateKey,
+    publicKey: envPublicKey,
+    limit: envLimit
+  }
+
+  return validatedEnvVariables
+}
+
+export default validateEnvVariables
