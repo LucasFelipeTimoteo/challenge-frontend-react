08e1b5404a243921eb1cdda1c894b1d2add11a79
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 21:06:56 -0300

    Create ResultsNotFound part styles

---
+++ frontend/src/parts/GLOBAL/Loading/styles.ts
@@ -0,0 +1,14 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  loadingContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontWeight: 800,
+    textShadow: '1px 1px 2px gray',
+    marginTop: 20
+  }
+})
+
+export default useStyles
