a43acfa0d3c59a4effaa3b2a49c626790cc772e2
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 21:13:18 -0300

    Create getEnvVariables util function

---
+++ frontend/src/hooks/utils/getEnvVariables.ts
@@ -0,0 +1,21 @@
+import validateEnvVariables from "../errors/validateEnvVariables"
+
+const getEnvVariables = () => {
+  const envLimit = process.env.REACT_APP_API_LIMIT
+  const envPrivateKey = process.env.REACT_APP_API_PRIVATE_KEY
+  const envPublicKey = process.env.REACT_APP_API_PUBLIC_KEY
+
+  const {
+    privateKey,
+    publicKey,
+    limit
+  } = validateEnvVariables({ envPrivateKey, envPublicKey, envLimit })
+
+  return {
+    privateKey,
+    publicKey,
+    limit
+  }
+}
+
+export default getEnvVariables
