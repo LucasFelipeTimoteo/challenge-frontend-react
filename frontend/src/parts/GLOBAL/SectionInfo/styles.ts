76d9e1d6a777d7df42521bc5c14565181dabc4af
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 21:03:00 -0300

    Update SectionInfoContainer

--- frontend/src/parts/GLOBAL/SectionInfo/styles.ts
+++ frontend/src/parts/GLOBAL/SectionInfo/styles.ts
@@ -1,17 +1,31 @@
 import { makeStyles } from "@material-ui/core";
 import { symmetricSideMarginStyleVar } from "../../../themes/globalStyleVariables";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(({ breakpoints }) => ({
   sectionInfoContainer: {
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    margin: `34px ${symmetricSideMarginStyleVar}`
+    gap: 35,
+    margin: `34px ${symmetricSideMarginStyleVar}`,
+
+    [breakpoints.down(breakpoints.values.md)]: {
+      flexDirection: 'column',
+      gap: 10
+    }
   },
 
   conditionalSectionInfoCharacterPage: {
     margin: `100px ${symmetricSideMarginStyleVar} 34px`
+  },
+
+  loadMoredataSwitchLabel: {
+    margin: 0,
+
+    [breakpoints.down(breakpoints.values.md)]: {
+      order: 1
+    }
   }
-})
+}))
 
 export default useStyles
