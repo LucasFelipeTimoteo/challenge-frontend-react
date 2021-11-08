d56c7954e2816ac50a9e2df0de28a14544808ba8
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-07 21:12:42 -0300

    Add correct props to components inside Characters page

--- frontend/src/pages/Characters/index.tsx
+++ frontend/src/pages/Characters/index.tsx
@@ -1,20 +1,71 @@
 import React from 'react'
+import { useInView } from 'react-intersection-observer'
 import CharactersList from '../../components/CharactersList'
 import Header from '../../components/Header'
 import Hero from '../../components/Hero'
+import useCharacters from '../../hooks/useCharacters'
+import useLoadMoreData from '../../hooks/useLoadMoreData'
+import useSearch from '../../hooks/useSearch/intex'
+import useSearchCharactersList from '../../hooks/useSearchCharactersList'
 import Searchbar from '../../parts/CharactersList/Searchbar'
+import Loading from '../../parts/GLOBAL/Loading'
+import ResultsNotFound from '../../parts/GLOBAL/ResultsNotFound'
 import SectionInfo from '../../parts/GLOBAL/SectionInfo'
 
+
 export default function Characters() {
+  const { searchValue, handleSearchValue, searchKey, handleSearchKey } = useSearch()
+  const { ref, inView } = useInView()
+  const { loadMoreData, toggleLoadMoreData } = useLoadMoreData()
+  const { searchCharactersList, totalSearchResults, loadingSearchCharacters } = useSearchCharactersList({ searchKey })
+  const { characters, loadingCharacters, totalResults } = useCharacters(inView, loadMoreData, searchKey)
+
+  const searchResultsNotFound = Boolean(searchCharactersList.length === 0 && searchKey && !loadingSearchCharacters)
+  const currentCharactersList = (
+    searchCharactersList.length > 0 && searchKey ? searchCharactersList : characters
+  )
+
   return (
     <>
       <Header />
       <Hero charactersPage>
-        <Searchbar />
+        <Searchbar
+          searchValue={searchValue}
+          handleSearchValue={handleSearchValue}
+          handleSearchKey={handleSearchKey}
+        />
       </Hero>
 
-      <SectionInfo charactersPage />
-      <CharactersList />
+      <SectionInfo
+        charactersPage
+        totalSearchResults={totalSearchResults}
+        totalResults={totalResults}
+        searchResultsNotFound={searchResultsNotFound}
+        loadingSearchCharacters={loadingSearchCharacters}
+        loadMoreData={loadMoreData}
+        toggleLoadMoreData={toggleLoadMoreData}
+        searchKey={searchKey}
+      />
+
+      <CharactersList
+        currentCharactersList={currentCharactersList}
+        searchResultsNotFound={searchResultsNotFound}
+        characters={characters}
+        loadingSearchCharacters={loadingSearchCharacters}
+        ref={ref}
+      />
+
+      {
+        ((loadingCharacters || loadingSearchCharacters) && !searchResultsNotFound) && (
+          <Loading />
+        )
+      }
+
+      {
+        searchResultsNotFound && (
+          <ResultsNotFound />
+        )
+      }
     </>
   )
 }
