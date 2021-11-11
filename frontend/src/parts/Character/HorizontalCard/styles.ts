4acf976ffc87876e86940ac2a83ac41e91eea9b1
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-10 21:30:33 -0300

    Fix HorizontalCardMedia style Improve resposiveness and remove justifyContent

--- frontend/src/parts/Character/HorizontalCard/styles.ts
+++ frontend/src/parts/Character/HorizontalCard/styles.ts
@@ -1,5 +1,7 @@
 import { makeStyles } from "@material-ui/core";
 
+import { cardScrollbarStyleObjectVar } from '../../../themes/globalStyleVariables'
+
 const useStyles = makeStyles(({ breakpoints }) => ({
   horizontalCardContainer: {
     position: 'relative',
@@ -7,7 +9,6 @@ const useStyles = makeStyles(({ breakpoints }) => ({
     width: 'min(1170px, 100%)',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
     borderRadius: '10px 50px',
     boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
     gap: 'clamp(1px, 3vw, 50px)',
@@ -20,10 +21,15 @@ const useStyles = makeStyles(({ breakpoints }) => ({
   },
 
   horizontalCardMedia: {
-    minWidth: 'clamp(134px, 20vw, 200px)',
-    minHeight: 'clamp(132.9px, 20vw, 188.09px)',
+    minWidth: 'clamp(134px, 24vw, 200px)',
+    minHeight: 'clamp(132.9px, 24vw, 188.09px)',
     borderRadius: '50%',
     marginLeft: 'clamp(6px, 3.7vw, 50px)',
+    zIndex: 3,
+
+    [breakpoints.down(breakpoints.values.md)]: {
+      transform: 'translateY(-30px)'
+    },
 
     [breakpoints.down(breakpoints.values.sm)]: {
       clipPath: 'circle(0%)',
@@ -34,8 +40,7 @@ const useStyles = makeStyles(({ breakpoints }) => ({
       minWidth: '100%',
       margin: 0,
       borderRadius: 0,
-      zIndex: 1
-    },    
+    },
   },
 
   conditionalCardImageFocusedHorizontalCardMedia: {
@@ -69,16 +74,26 @@ const useStyles = makeStyles(({ breakpoints }) => ({
   },
 
   horizontalCardInfo: {
+    position: 'relative',
     textAlign: 'left',
     display: 'flex',
     flexDirection: 'column',
-    margin: '0 min(70px, 5.3vw) 0 0',
-    padding: 0,
+    padding: '30px 0 60px',
     gap: 'clamp(1px, 2vw, 20px)',
+    height: '100%',
+    overflow: 'auto',
+    margin: '40px 20px 2px',
+
+    ...cardScrollbarStyleObjectVar,
 
     [breakpoints.down(breakpoints.values.md)]: {
-      margin: '30px 20px 2px'
+      margin: '0 min(70px, 5.3vw) 0 20px',
     }
+  },
+
+  horizontalCardDescription: {
+    paddingBottom: 50,
+    marginRight: 8
   },
 
   conditionalComicHorizontalCardInfo: {
