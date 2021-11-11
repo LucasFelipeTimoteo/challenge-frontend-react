0a3f3696a01ef3e85e671a7b7f0b0b1494e097a0
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-10 21:48:26 -0300

    Update CharactersList props on CharactersList

--- frontend/src/pages/Team/index.tsx
+++ frontend/src/pages/Team/index.tsx
@@ -2,13 +2,19 @@ import React from 'react'
 import CharactersList from '../../components/CharactersList'
 import Header from '../../components/Header'
 import Hero from '../../components/Hero'
+import { useFavoriteCharacterProvider } from '../../contexts/favoritedCharacters'
 
 export default function Team() {
+  const { favoriteCharacters } = useFavoriteCharacterProvider()
+
   return (
     <>
       <Header teamPage />
       <Hero teamPage />
-      <CharactersList favorited teamPage />
+      <CharactersList
+        currentCharactersList={favoriteCharacters}
+        teamPage
+      />
     </>
   )
 }
