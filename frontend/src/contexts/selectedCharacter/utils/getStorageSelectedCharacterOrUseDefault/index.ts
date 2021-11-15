660d375fa278cf7af69f5560fe4bbcd0fef93aba
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-15 19:28:03 -0300

    Create getStorageSelectedCharacterOrUseDefault util function

---
+++ frontend/src/contexts/selectedCharacter/utils/getStorageSelectedCharacterOrUseDefault/index.ts
@@ -0,0 +1,21 @@
+import { ICharacter } from "../../../../hooks/useCharacters/types"
+import singleCharacterTypeGuard from "../typeGuards/singleCharacterTypeGuard"
+
+let defaultCharacterValue = {} as ICharacter
+
+const getStorageSelectedCharacterOrUseDefault = () => {
+  const storageCharacters = window.localStorage.getItem('MARVEL_STRIKE_TEAM_SELECTED_CHARACTER')
+  if (!storageCharacters) {
+    return defaultCharacterValue
+  }
+
+  const parsedStorageCharacter = JSON.parse(storageCharacters)
+  
+  if(singleCharacterTypeGuard(parsedStorageCharacter)){
+    return parsedStorageCharacter
+  } else {
+    return defaultCharacterValue
+  }
+}
+
+export default getStorageSelectedCharacterOrUseDefault
