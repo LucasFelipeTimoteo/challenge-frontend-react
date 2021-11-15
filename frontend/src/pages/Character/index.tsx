4a7a8da7133d85102a24245c09e7666091cfdcc4
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-15 19:12:51 -0300

    Add Loading screen in Character page

--- frontend/src/pages/Character/index.tsx
+++ frontend/src/pages/Character/index.tsx
@@ -5,14 +5,16 @@ import Hero from '../../components/Hero'
 import { useSeletedUser } from '../../contexts/selectedCharacter'
 import useCharacterComics from '../../hooks/useCharacterComics'
 import HorizontalCard from '../../parts/Character/HorizontalCard'
+import Loading from '../../parts/GLOBAL/Loading'
 import SectionInfo from '../../parts/GLOBAL/SectionInfo'
 
 export default function Character() {
-  const { selectedCharacter } = useSeletedUser()
+  const { selectedCharacter, loadingSelectedCharacter } = useSeletedUser()
 
   const {
     characterComics,
     characterComicsCount,
+    loadingCharacterComics
   } = useCharacterComics(selectedCharacter.id)
 
   return (
@@ -25,6 +27,9 @@ export default function Character() {
       <CharacterInfo
         characterComics={characterComics}
       />
+      {(loadingCharacterComics || loadingSelectedCharacter) && (
+        <Loading />
+      )}
     </>
   )
 }
