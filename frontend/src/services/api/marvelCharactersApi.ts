1c9f218afa0900cc63cf3c2eff4507c648e9d1e7
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-15 19:11:09 -0300

    Create marvelCharactersApi

---
+++ frontend/src/services/api/marvelCharactersApi.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+interface ImarvelCharactersApiProps {
+  limit: string
+  timestamp: string
+  publicKey: string
+  hash: () => string,
+  handleOffset?: () => string
+  nameStartsWith?: string
+  characterId?: number
+}
+
+export default function marvelCharactersApi({
+  limit,
+  handleOffset,
+  timestamp,
+  publicKey,
+  hash,
+  nameStartsWith,
+  characterId
+}: ImarvelCharactersApiProps) {
+
+  console.log(characterId)
+  const api = axios.create({
+    method: 'GET',
+    baseURL: 'http://gateway.marvel.com/v1/public/',
+    params: {
+      limit,
+      ts: timestamp,
+      apikey: publicKey,
+      hash: hash(),
+      ...(handleOffset && { offset: handleOffset() }),
+      ...(nameStartsWith && { nameStartsWith }),
+    }
+  })
+
+  return api
+}
