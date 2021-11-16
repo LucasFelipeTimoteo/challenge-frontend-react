618c93411e1428c81612f4f32de62d04c6937bdf
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-15 23:15:32 -0300

    storageList parameter receive type any and add checking arrays in charactersListTypeGuard
    - now, storageList recevei type any intead any[]
    - checking if receveid storageList is array in conditional

--- frontend/src/hooks/utils/typeGuards/charactersListTypeGuard/index.ts
+++ frontend/src/hooks/utils/typeGuards/charactersListTypeGuard/index.ts
@@ -1,10 +1,15 @@
 import { ICharacter } from "../../../useCharacters/types"
 
-const charactersListTypeGuard = (storageList: any[]): storageList is ICharacter[] => {
+const charactersListTypeGuard = (storageList: any): storageList is ICharacter[] => {
+  if(!Array.isArray(storageList)) {
+    return false
+  }
+
   if (!(typeof storageList[0] === 'object' && storageList[0] !== null)) {
     return false
   }
-  else if ('id' in storageList[0] && 'name' in storageList[0]) {
+  
+  if ('id' in storageList[0] && 'name' in storageList[0]) {
     return true
   }
 
