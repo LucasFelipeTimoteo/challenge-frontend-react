20fa61d1961efa47559c8d3a81c02acb7da9bec4
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-16 11:19:35 -0300

    Create characterComicsTypeGuard type guard

---
+++ frontend/src/hooks/useCharacterComics/utils/characterComicsTypeGuard/index.ts
@@ -0,0 +1,25 @@
+import { ICharacterComics } from "../../types";
+
+const characterComicsTypeGuard = (characterComicsList: any): characterComicsList is ICharacterComics[] => {
+  if (!Array.isArray(characterComicsList)) {
+    return false
+  }
+
+  if (!(typeof characterComicsList[0] === 'object' && characterComicsList[0] !== null)) {
+    return false
+  }
+
+  if (
+    !(
+      "id" in characterComicsList[0] &&
+      "title" in characterComicsList[0] &&
+      "dates" in characterComicsList[0]
+    )
+  ) {
+    return false
+  }
+
+  return true
+}
+
+export default characterComicsTypeGuard
