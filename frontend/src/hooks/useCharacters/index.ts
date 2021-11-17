e7ea7d4be4cf638a58c918074b83342e385ec943
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-16 21:34:43 -0300

    Update import of getStorageResultsNumberOrUseDefault from getStorageResultsNumberOrUseDefault to getStorageCharactersResultsCountOrUseDefault

--- frontend/src/hooks/useCharacters/index.ts
+++ frontend/src/hooks/useCharacters/index.ts
@@ -4,7 +4,8 @@ import marvelCharactersApi from "../../services/api/marvelCharactersApi"
 import getEnvVariables from "../utils/getEnvVariables"
 import charactersListTypeGuard from "./utils/typeGuards/charactersListTypeGuard"
 import getStorageCharactersOrUseDefault from './utils/getStorageCharactersOrUseDefault'
-import getStorageResultsNumberOrUseDefault from "./utils/getStorageResultsNumberOrUseDefault"
+import getStorageCharactersResultsCountOrUseDefault from "./utils/getStorageCharactersResultsCountOrUseDefault"
+import setLocalStorageData from "../../utils/setListToLocalStorage"
 
 export default function useCharacters(
   inView: boolean,
@@ -12,7 +13,7 @@ export default function useCharacters(
   searchKey: string
 ) {
   const defaultCharactersValue = getStorageCharactersOrUseDefault()
-  const defaultCharactersCountValue = getStorageResultsNumberOrUseDefault()
+  const defaultCharactersCountValue = getStorageCharactersResultsCountOrUseDefault()
 
   const [characters, setCharacters] = useState(defaultCharactersValue)
   const [charactersCount, setcharactersCount] = useState(defaultCharactersCountValue)
@@ -48,23 +49,17 @@ export default function useCharacters(
         const response = apiRequest.data
         const resultsNumber = response.data.total
         const characterList = response.data.results
-
+        
         if (!charactersListTypeGuard(characterList)) {
           throw new Error('API response does not have correct type')
         }
-
+        const newCharacterList = [...characters, ...characterList]
+        
         setCharacters(prev => [...prev, ...characterList])
         setcharactersCount(resultsNumber)
         handleOffset()
-        window.localStorage.setItem(
-          'MARVEL_STRIKE_TEAM_FETCHED_CHARACTERS',
-          JSON.stringify([...characters, ...characterList])
-        )
-
-        window.localStorage.setItem(
-          'MARVEL_STRIKE_TEAM_FETCHED_RESULTS_NUMBER',
-          resultsNumber
-        )
+        setLocalStorageData('characters', {list: newCharacterList})
+        setLocalStorageData('charactersCount', {item: resultsNumber})
 
         setLoadingCharacters(false)
       }
