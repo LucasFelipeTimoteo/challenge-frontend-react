239e8cafa9d15fb77b56ad1911e176c3fde0b692
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-19 17:43:45 -0300

    Update toggleThemeButton styles Change color whwn breakpoint reached and new style to button wrapper

--- frontend/src/parts/GLOBAL/ToggleThemeButton/index.tsx
+++ frontend/src/parts/GLOBAL/ToggleThemeButton/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
 import { IconButton } from '@material-ui/core'
 import { Brightness4, Brightness7 } from '@material-ui/icons'
+import React from 'react'
 import { useMUITheme } from '../../../styles/themes/MUITheme'
 import useStyles from './styles'
 
 export default function ToggleThemeButton() {
   const { togglepageTheme, pageTheme } = useMUITheme()
-  const { toggleThemeButton } = useStyles()
+  const { toggleThemeButton, toggleThemeButtonWrapper } = useStyles()
 
   return (
-    <IconButton onClick={togglepageTheme}>
+    <IconButton onClick={togglepageTheme} className={toggleThemeButtonWrapper}>
       {
         pageTheme === "light"
           ?
--- frontend/src/parts/GLOBAL/ToggleThemeButton/styles.ts
+++ frontend/src/parts/GLOBAL/ToggleThemeButton/styles.ts
@@ -1,14 +1,21 @@
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles(({ breakpoints }) => ({
+const useStyles = makeStyles(({ breakpoints, palette }) => ({
   toggleThemeButton: {
     fontSize: '3rem',
     color: 'whitesmoke',
-
+    
     [breakpoints.down(breakpoints.values.md)]: {
-      fontSize: '2.5rem'
-    }
-  }
+      fontSize: '2.5rem',
+      color: palette.primary.main
+    },
+  },
+  
+  toggleThemeButtonWrapper: {
+    margin: 0,
+    marginLeft: 'auto',
+    padding: 0,
+  },
 }))
 
 export default useStyles
