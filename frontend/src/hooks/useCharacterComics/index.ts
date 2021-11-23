7fc7bf178106f97ae598fc22e4e49218bc3f776e
Lucas Felipe <conhecimento54321@gmail.com>
2021-11-22 21:23:06 -0300

    Update fetch condition, parameters and add new method to useCharacterComics hook
    - new parameter: inView
    - new method: handleOffset
    - fetch conditions: firstRenderFetchCondition and normalFetchCondition

--- frontend/src/hooks/useCharacterComics/index.ts
+++ frontend/src/hooks/useCharacterComics/index.ts
@@ -5,10 +5,13 @@ import getEnvVariables from "../utils/getEnvVariables";
 import { ICharacterComics } from "./types";
 import characterComicsTypeGuard from "./utils/characterComicsTypeGuard";
 
-export default function useCharacterComics(characterId: number) {
+export default function useCharacterComics(characterId: number, inView: boolean) {
   const [characterComics, setCharacterComics] = useState<ICharacterComics[]>([])
   const [characterComicsCount, setCharacterComicsCount] = useState(0)
   const [loadingCharacterComics, setLoadingCharacterComics] = useState(false)
+
+  const firstRenderFetchCondition = (characterId && characterComics.length === 0 && !loadingCharacterComics)
+  const normalFetchCondition = (characterId && inView && !loadingCharacterComics)
 
   useEffect(() => {
     const getCharacterComic = async () => {
@@ -18,26 +21,39 @@ export default function useCharacterComics(characterId: number) {
       const { privateKey, publicKey, limit } = getEnvVariables()
       const hash = md5(timestamp + privateKey + publicKey)
 
+      const handleOffset = () => {
+        const totalPageCharacters = characterComics.length
+        const newOffset = String(totalPageCharacters)
+        return newOffset
+      }
+
       try {
         const api = marvelComicsApi({
           limit,
           publicKey,
           hash,
           timestamp,
-          characterId: String(characterId)
+          characterId: String(characterId),
+          handleOffset
         })
+
         const apiRequest = await api('comics')
 
         const response = apiRequest.data
         const characterComicsCount = response.data.count
         const characterComicsList = response.data.results
 
-        if(!characterComicsTypeGuard(characterComicsList)) {
+        if (!characterComicsTypeGuard(characterComicsList)) {
           throw new Error('API response does not have correct type')
         }
 
-        setCharacterComics(prev => [...prev, ...characterComicsList])
+        const characterComicsListWithoutDuplicates = characterComicsList.filter(responseComic => (
+          characterComics.every(comic => responseComic.id !== comic.id)
+        ))
+
+        setCharacterComics(prev => [...prev, ...characterComicsListWithoutDuplicates])
         setCharacterComicsCount(characterComicsCount)
+
         setLoadingCharacterComics(false)
       }
       catch (error) {
@@ -45,10 +61,10 @@ export default function useCharacterComics(characterId: number) {
       }
     }
 
-    if (characterId) {
+    if (firstRenderFetchCondition || normalFetchCondition) {
       getCharacterComic()
     }
-  }, [characterId])
+  }, [characterId, firstRenderFetchCondition, normalFetchCondition, characterComics])
 
   return {
     characterComics,
